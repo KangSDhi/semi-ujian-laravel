fc36e6bec5040f82a33fd962f0ddfdd18be34b3e
KangSDhi <kangteknisi@gmail.com>
2024-02-04 19:42:07 +0700

    feat(siswa): fix data soal siswa

--- app/Http/Controllers/API/Soal/SoalController.php
+++ app/Http/Controllers/API/Soal/SoalController.php
@@ -14,24 +14,25 @@ class SoalController extends Controller
 {
     public function getSoalByUser(){
         $getUser = auth('jwt')->user();
-        dd($getUser->kelas->jurusan_id);
-        // $getToday = date("Y-m-d");
+        $getToday = date("Y-m-d");
 
-        // $getSoal = DB::table(function($query) use ($getToday){
-        //     $query->select(DB::raw('id, nama_soal, link, jurusan_id, waktu_mulai'))
-        //         ->from('soal')
-        //         ->where('waktu_mulai', 'LIKE', $getToday.'%');
-        // })->where(function($query) use ($getUser){
-        //     $query->whereNull('jurusan_id')
-        //         ->orWhere('jurusan_id', '=', $getUser->jurusan_id);
-        // })
-        // ->orderBy('waktu_mulai', 'asc')
-        // ->get();
+        $getSoal = DB::table(function($query) use ($getToday){
+            $query->select(DB::raw('id, nama_soal, link, jurusan_id, tingkat_id, waktu_mulai'))
+                ->from('soal')
+                ->where('waktu_mulai', 'LIKE', $getToday.'%');
+        })->where(function($query) use ($getUser){
+            $query->where('tingkat_id', '=', $getUser->kelas->tingkat_id);
+        })->where(function($query) use ($getUser){
+            $query->whereNull('jurusan_id')
+                ->orWhere('jurusan_id', '=', $getUser->kelas->jurusan_id);
+        })
+        ->orderBy('waktu_mulai', 'asc')
+        ->get();
 
-        // return response()->json([
-        //     "code"  => 200,
-        //     "data"  => $getSoal
-        // ], 200);
+        return response()->json([
+            "code"  => 200,
+            "data"  => $getSoal
+        ], 200);
     }
 
     public function getSoalJoinJurusanTingkat(){
